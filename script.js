d8087a57355f7f8959574b3b3cafefef5b337408
Paulina <paulina.05.krol@gmail.com>
2021-08-29 17:28:32 +0200

    html, js: page navigation implemented

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const section1 = document.querySelector('#section--1');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
 
 const openModal = e => {
   e.preventDefault();
@@ -28,10 +30,44 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
 });
 
-// Scrolling
-const section1 = document.querySelector('#section--1');
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-
+/////////////////////////////////////////////////////////
+// Button scrolling
 btnScrollTo.addEventListener('click', () => {
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/////////////////////////////////////////////////////////
+// Page navigation
+const sectionsAll = document.querySelectorAll('.section');
+
+// document.querySelectorAll('.nav__link').forEach(function (el, i) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     // const section = document.querySelector(`#section--${i + 1}`);
+//     // section?.scrollIntoView({ behavior: 'smooth' });
+
+//     const id = this.getAttribute('href');
+//     console.log(id);
+
+//     console.log(document.querySelector(id));
+//     document.querySelector(id)?.scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Added event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (
+    e.target.classList.contains('nav__link') &&
+    !e.target.classList.contains('nav__link--btn')
+  ) {
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id)?.scrollIntoView({ behavior: 'smooth' });
+  }
 });
