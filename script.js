232706c78246e1d62b8bc42051470c1c766f9939
Paulina <paulina.05.krol@gmail.com>
2021-08-31 23:32:52 +0200

    css, js: revealing elements on scroll implemented (Intersection Observer API)

--- script.js
+++ script.js
@@ -163,3 +163,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+///////////////////////////////////////////////////////////
+// Revealing Elements on Scroll
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
